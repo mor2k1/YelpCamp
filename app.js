2acc98bd369687132420525dd77de5c9dbc11d3b
Mor <yuvi.mor@gmail.com>
2017-11-27 18:54:21 +0000

    add db url

--- app.js
+++ app.js
@@ -22,7 +22,10 @@ seedDB();
 
 mongoose.Promise = global.Promise; // for an error message in the terminal. 
 
+
 // mongoose.connect("mongodb://localhost/yelp_camp_v12", {useMongoClient: true}); 
+// export DATABASEURL=mongodb://localhost/yelp_camp_v12  - in the terminal.
+mongoose.connect(process.env.DATABASEURL, {useMongoClient: true});
 mongoose.connect("mongodb://mor:3122pass@ds123146.mlab.com:23146/yelp_camp_mor", {useMongoClient: true}); 
 
 
